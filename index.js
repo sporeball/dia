c6984471d87a57ea0f84758f649084fd1338862c
sporeball <sporeballdev@gmail.com>
2025-01-14 16:57:18 -0500

    more tech

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
 import { fileStem, writeFile } from './util.js';
+import stripIndent from 'strip-indent';
 
 let rules = {};
 
-let DIA_RULE_SIMPLE = function(tag, code) {
-  return code.replaceAll(tag, tag.slice(4));
+let DIA_RULE_SIMPLE = function(tagName, code) {
+  return code.replaceAll(tagName, tagName.slice(4));
+}
+
+function getMatchingTags(tagName, code) {
+  const ex = getGlobalMatchingTagsExpression(tagName);
+  return Array.from(code.matchAll(ex), match => {
+    return {
+      tag: match[0],
+      attributes: match[1],
+      content: match[2],
+    };
+  });
+}
+
+function getMatchingTagsCount(tagName, code) {
+  return getMatchingTags(tagName, code).length;
+}
+
+function getMatchingTagsExpression(tagName) {
+  return new RegExp(`<${tagName}([^<>]*)>(.*)</${tagName}>`);
+}
+
+function getGlobalMatchingTagsExpression(tagName) {
+  return new RegExp(`<${tagName}([^<>]*)>(.*)</${tagName}>`, 'g');
+}
+
+function getFirstMatchingTag(tagName, code) {
+  const matchingTags = getMatchingTags(tagName, code);
+  return matchingTags[0];
+}
+
+function getLastMatchingTag(tagName, code) {
+  const matchingTags = getMatchingTags(tagName, code);
+  return matchingTags.at(-1);
+}
+
+function removeDuplicateTags(tagName, code) {
+  const matchingTagsExpression = getMatchingTagsExpression(tagName);
+  const matchingTagsCount = getMatchingTagsCount(tagName, code);
+  for (let i = 0; i < matchingTagsCount; i++) {
+    if (i == matchingTagsCount - 1) {
+    } else {
+      code = code.replace(matchingTagsExpression, '');
+    }
+  }
 }
 
 /**
  * add a rule to the rules list
- * @param {string} tag
+ * @param {string} tagName
  * @param {function} cb
  */
-function addRule(tag, cb) {
-  rules[tag] = cb;
+function addRule(tagName, cb) {
+  rules[tagName] = cb;
 }
 
 /**
@@ -21,8 +66,8 @@ function addRule(tag, cb) {
  * @returns {string}
  */
 function followRules(code) {
-  for (const [tag, cb] of Object.entries(rules)) {
-    code = cb(tag, code);
+  for (const [tagName, cb] of Object.entries(rules)) {
+    code = cb(tagName, code);
   }
   return code;
 }
