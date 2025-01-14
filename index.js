66279eded8c6de78fe55df4d37df99fd6884d91c
sporeball <sporeballdev@gmail.com>
2025-01-14 15:18:27 -0500

    rules

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 import { fileStem, writeFile } from './util.js';
 
+let rules = {};
+
+let DIA_RULE_SIMPLE = function(tag, code) {
+  return code.replaceAll(tag, tag.slice(4));
+}
+
+/**
+ * add a rule to the rules list
+ * @param {string} tag
+ * @param {function} cb
+ */
+function addRule(tag, cb) {
+  rules[tag] = cb;
+}
+
+/**
+ * process dia code according to the rules list
+ * @param {string} code
+ * @returns {string}
+ */
+function followRules(code) {
+  for (const [tag, cb] of Object.entries(rules)) {
+    code = cb(tag, code);
+  }
+  return code;
+}
+
+addRule('dia-head', DIA_RULE_SIMPLE);
+addRule('dia-title', DIA_RULE_SIMPLE);
+addRule('dia-style', DIA_RULE_SIMPLE);
+
 /**
  * generate a slide deck from dia code
  * @param {string} filename
@@ -7,5 +38,6 @@ import { fileStem, writeFile } from './util.js';
  */
 export default function generateSlides (filename, code) {
   let stem = fileStem(filename);
+  code = followRules(code);
   writeFile(`${stem}.html`, code);
 }
