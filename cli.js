7bcb2199a40ba1792781212989377321f4ca8751
sporeball <sporeballdev@gmail.com>
2025-01-16 02:16:16 -0500

    watch

--- cli.js
+++ cli.js
@@ -2,15 +2,32 @@
 
 import generateSlides from './index.js';
 import { fileStem, readDiaFile } from './util.js';
+import fs from 'node:fs';
 import process from 'node:process';
 import colors from 'picocolors';
 
 function cli () {
   let filename = process.argv[2];
+  let watch = process.argv.includes('--watch');
   let stem = fileStem(filename);
   console.log(`dia ${colors.cyan('(https://github.com/sporeball/dia)')}`);
-  generateSlides(filename, readDiaFile(filename));
-  console.log(`  ${colors.green('o')} wrote slide deck to ${stem}.html`);
+  if (watch) {
+    fs.watch(filename, (eventType, filename) => {
+      if (eventType === 'change') {
+        generateSlides(filename, readDiaFile(filename));
+        const time = (new Date).toLocaleTimeString(
+          [],
+          {hour: 'numeric', minute: 'numeric', second: 'numeric'}
+        )
+          .replace(' AM', '')
+          .replace(' PM', '');
+        console.log(`  ${colors.green('o')} wrote slide deck to ${stem}.html (${time})`);
+      }
+    });
+  } else {
+    generateSlides(filename, readDiaFile(filename));
+    console.log(`  ${colors.green('o')} wrote slide deck to ${stem}.html`);
+  }
 }
 
 try {
