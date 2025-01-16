7334a8c1be457b5c00d4b2b11275626b76e34d34
sporeball <sporeballdev@gmail.com>
2025-01-16 00:53:34 -0500

    slide url parameter

--- dia.js
+++ dia.js
@@ -1,19 +1,33 @@
-let active_slide = 1;
+let active_slide;
 let num_slides = document.querySelectorAll('.dia-slide').length;
+let urlParameters = new URLSearchParams(window.location.search);
 
-document.getElementById('dia-slide-1').classList.add('dia-active');
+window.addEventListener('DOMContentLoaded', e => {
+  active_slide = urlParameters.get('slide');
+  if (active_slide === null) {
+    active_slide = 1;
+    urlParameters.set('slide', active_slide);
+    window.location.search = urlParameters;
+  }
+  document.getElementById(`dia-slide-${active_slide}`).classList.add('dia-active');
+});
 
 document.addEventListener('keydown', function(e) {
   if (e.key === 'ArrowLeft' || e.code === 'ArrowLeft') {
     if (active_slide > 1) {
       document.getElementById(`dia-slide-${active_slide}`).classList.remove('dia-active');
       document.getElementById(`dia-slide-${--active_slide}`).classList.add('dia-active');
+      urlParameters.set('slide', active_slide);
+      window.location.search = urlParameters;
     }
   }
   if (e.key === 'ArrowRight' || e.code === 'ArrowRight') {
     if (active_slide < num_slides) {
       document.getElementById(`dia-slide-${active_slide}`).classList.remove('dia-active');
       document.getElementById(`dia-slide-${++active_slide}`).classList.add('dia-active');
+      urlParameters.set('slide', active_slide);
+      window.location.search = urlParameters;
     }
   }
-})
+});
+
